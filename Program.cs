42cdabb976100d224040cb7e270c0a4b493be1ef
aadneu <aadneu@gmail.com>
2023-11-23 14:49:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Mime;
+
+Console.WriteLine("\n Velkommen til Hangman! \n______________________________________ \n");
+
+
+string currentWord = "hangman";
+
+void Run()
+{
+    foreach (char letter in currentWord)
+    {
+        
+    }
+}
