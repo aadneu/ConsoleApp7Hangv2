6c2863749e06ce60efa7761efc87c4c5701f27f3
aadneu <aadneu@gmail.com>
2023-11-25 12:34:05 +0100

    fungerene simpelt hangman

---
+++ DisplayHangman.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    public class DisplayHangman
+    {
+        string[] display = {
+            @"
+              +---+
+              |   |
+                  |
+                  |
+                  |
+                  |
+            =========",
+
+            @"
+              +---+
+              |   |
+              O   |
+                  |
+                  |
+                  |
+            =========",
+
+            @"
+              +---+
+              |   |
+              O   |
+              |   |
+                  |
+                  |
+            =========",
+
+            @"
+              +---+
+              |   |
+              O   |
+             /|   |
+                  |
+                  |
+            =========",
+
+            @"
+              +---+
+              |   |
+              O   |
+             /|\  |
+                  |
+                  |
+            =========",
+
+            @"
+              +---+
+              |   |
+              O   |
+             /|\  |
+             /    |
+                  |
+            =========",
+
+            @"
+              +---+
+              |   |
+              O   |
+             /|\  |
+             / \  |
+                  |
+            ========="
+            
+        };
+        public void Show(int i){
+          
+            Console.WriteLine(display[i]);
+            
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,71 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Net.Mime;
+using ConsoleApp7;
 
+var display = new DisplayHangman();
 Console.WriteLine("\n Velkommen til Hangman! \n______________________________________ \n");
-
-
 string currentWord = "hangman";
+string displayWord = new string('*', currentWord.Length);
+int guesses = 0;
+
+int antallBokstaver = currentWord.Length;
+Console.WriteLine($"Ordet vi skal frem til inneholder {antallBokstaver} bokstaver.\n");
+
+
+while (displayWord != currentWord)
+{
+    Run();
+}
+Console.WriteLine("Gratulerer du vant!");
+
 
 void Run()
 {
-    foreach (char letter in currentWord)
+    Console.WriteLine(displayWord);
+    var input = Console.ReadLine()[0];
+    
+  
+    
+    for (int i = 0; i < currentWord.Length; i++)
     {
-        
+        if (input == currentWord[i])
+        {
+            displayWord = displayWord.Substring(0, i) + input + displayWord.Substring(i + 1);
+           
+        } 
+
     }
+    Console.SetCursorPosition(0, 6);
+
+
 }
+/*
+void Run()
+{
+
+    var input = Console.ReadLine()[0];
+    Console.WriteLine(displayWord);
+    bool correctGuess = false;
+
+    for (int i = 0; i < currentWord.Length; i++)
+    {
+        if (input == currentWord[i])
+        {
+            displayWord = displayWord.Substring(0, i) + input + displayWord.Substring(i + 1);
+            correctGuess = true;
+        }
+
+    }
+
+    if (!correctGuess)
+    {
+        guesses++;
+        Console.WriteLine(guesses);
+    }
+    display.Show(guesses);
+    Console.SetCursorPosition(0, 6);
+
+}
+
+*/
